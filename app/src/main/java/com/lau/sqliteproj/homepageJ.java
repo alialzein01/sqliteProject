b0cbdb00d9ccba7593df21fa7f75059e9a32b1b4
Ali Alzein <aly.alzein01@gmail.com>
2022-05-05 20:09:00 +0300

    adding images

---
+++ app/src/main/java/com/lau/sqliteproj/homepageJ.java
@@ -0,0 +1,14 @@
+package com.lau.sqliteproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homepageJ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage);
+    }
+}
