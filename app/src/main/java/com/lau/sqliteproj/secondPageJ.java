2d22d7801e51ca5c46b4c46414f7f88d8602a3a1
Ali Alzein <aly.alzein01@gmail.com>
2022-05-05 20:56:33 +0300

    adding java code on the webpage class file

--- app/src/main/java/com/lau/sqliteproj/secondPageJ.java
+++ app/src/main/java/com/lau/sqliteproj/secondPageJ.java
@@ -3,12 +3,19 @@ package com.lau.sqliteproj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class secondPageJ extends AppCompatActivity {
-
+    WebView w;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.secondpage);
+        w = (WebView) findViewById(R.id.webv);
+        // getting the specified url from the previous page
+        String wURL = getIntent().getStringExtra("URL");
+        w.setWebViewClient(new WebViewClient());
+        w.loadUrl(wURL);
     }
 }
