3389e14f0190b7f56864f6a6b2796b6a1797ef91
Ali Alzein <aly.alzein01@gmail.com>
2022-05-05 20:30:49 +0300

    adding sql commands in java code

--- app/src/main/java/com/lau/sqliteproj/homepageJ.java
+++ app/src/main/java/com/lau/sqliteproj/homepageJ.java
@@ -7,10 +7,22 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class homepageJ extends AppCompatActivity {
-    SQLiteDatabase sqlite;
+    SQLiteDatabase sqliteDB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
+        Intent i = new Intent(this, secondPageJ.class);
+        try {
+            sqliteDB = this.openOrCreateDatabase("fExamsdb", MODE_PRIVATE, null);
+            sqliteDB.execSQL("CREATE Table IF NOT EXISTS exams(subject VARCHAR, webpage VARCHAR)");
+            sqliteDB.execSQL("INSERT INTO exams(subject,webpage) VALUES ('Android Development','https://developer.android.com/docs')");
+            sqliteDB.execSQL("INSERT INTO exams(subject,webpage) VALUES ('Discrete Math','https://www.coursera.org/courses?query=discrete%20mathematics')");
+            sqliteDB.execSQL("INSERT INTO exams(subject,webpage) VALUES ('Operating System','https://maker.pro/linux/tutorial/basic-linux-commands-for-beginners')");
+            sqliteDB.execSQL("INSERT INTO exams(subject,webpage) VALUES ('Software Engineering','https://www.edx.org/learn/software-engineering')");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
